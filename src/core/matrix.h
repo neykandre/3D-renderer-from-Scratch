1c7ced1ba9b69a364adebd05c976f53d1cadb403
neykandre <andrejnosov.01.02.05@gmail.com>
2025-01-25 20:45:14 +0300

    add eigen

---
+++ src/core/matrix.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <Eigen/Dense>
+
+namespace renderer {
+    using Matrix4 = Eigen::Matrix4f;
+}
---
+++ src/core/vector.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <Eigen/Dense>
+
+namespace renderer {
+    using Vector4 = Eigen::Vector4f;
+    using Vector3 = Eigen::Vector3f;
+}
